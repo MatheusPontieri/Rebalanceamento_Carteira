9fddf13fef838c130370a988adf7c93a8e92c3f6
Matheus Pontieri <125307539+MatheusPontieri@users.noreply.github.com>
2023-03-17 09:37:26 -0300

    Mudança na estrutura (v1.1)

--- bolsa.py
+++ bolsa.py
@@ -1,83 +1,62 @@
-from time import sleep
 import yfinance as yf
+from time import sleep
+# Sempre manter yfinance atualizado
 
-sleep(1)
+# Define os ativos e as proporções
+ativos = ["HGLG11.SA", "HGRE11.SA", "HGFF11.SA", "HGPO11.SA", "WEGE3.SA", "PETR4.SA", "ITUB4.SA", "VALE3.SA"]
+proporcoes = [0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125]
 
-while True:
-    
-    print("Menu de Opções\n")
-    print("[1] - Fazer rebalanceamento da carteira")
-    print("[2] - Saber variação percentual dos ativos")
-    op = int(input("Digite a opção escolhida: "))
-    
-    while op == 1:
-    
-        patr_atual = float(input("\nDigite o patrimônio em Renda Variável: "))
-        aporte = float(input("Digite o valor do aporte: "))
-        ativos = int(input("Digite a quantidade de ativos: "))
-        pctg = (100/ativos)/100
-        patr_final = patr_atual + aporte
-        idealagr = patr_final*pctg
-        # Posso criar def que verifica o menor preço e faz rebalanceamento
-        while op == 1:
-            ticker = str(input("Digite o ticket do ativo com menor patrimônio no seguinte estilo (WEGE3F.SA):  "))
+# Quantidade de ações de cada tipo
+quantidades = [2, 2, 1, 3, 10, 6, 3, 8]
+quantidades1 = [2, 2, 1, 3, 10, 6, 3, 8]
 
-            ativo = yf.Ticker(ticker)
-            cot = ativo.history(period="1d")["Close"][0]
-            atual = float(input("Digite a quantidade de cotas desse ativo: "))
-            natual = atual*cot
-            investir = (idealagr - natual)/cot
-            print(investir)
-            investir1 = round(investir,2)
+# Pede o patrimônio atual e o valor do aporte
+valor_aporte = float(input("Digite o valor do aporte: "))
 
-            sleep(1)
-            print("\nVocê deve comprar",investir1,"cotas desse ativo!\n")
-            sleep(2)
-            op = int(input("O que deseja fazer?\n[1] Mais um rebalanceamento\n[2] Voltar ao menu inicial:\nOpção escolhida: "))
-            print("\n")
-        op = 3
-        
-    while op == 2:
-        patr_atual = float(input("Digite o patrimônio em Renda Variável: "))
-        ativos = int(input("Digite a quantidade de ativos: "))
-        pctg = (100/ativos)/100
-        idealants = patr_atual*pctg
+# Obtém as cotações atuais dos ativos
+current_prices = []
+for ticker in ativos:
+    stock = yf.Ticker(ticker)
+    current_price = stock.history(period='1d')['Close'][0]
+    current_prices.append(current_price)
     
-        hglg11 = float(input("Valor investido em HGLG11: "))
-    
-        hgre11 = float(input("Valor investido em HGRE11: "))
-    
-        hgff11 = float(input("Valor investido em HGFF11: "))
-    
-        hgpo11 = float(input("Valor investido em HGPO11: "))
-    
-        wege3 = float(input("Valor investido em WEGE3: "))
-    
-        petr4 = float(input("Valor investido em PETR4: "))
-    
-        itub4 = float(input("Valor investido em ITUB4: "))
-    
-        vale3 = float(input("Valor investido em VALE3: "))
-    
-        desvio_hglg11 = ((hglg11 - idealants)/patr_atual)*100
-        desvio_hgre11 = ((hgre11 - idealants)/patr_atual)*100
-        desvio_hgff11 = ((hgff11 - idealants)/patr_atual)*100
-        desvio_hgpo11 = ((hgpo11 - idealants)/patr_atual)*100
-        desvio_wege3 = ((wege3 - idealants)/patr_atual)*100
-        desvio_petr4 = ((petr4 - idealants)/patr_atual)*100
-        desvio_itub4 = ((itub4 - idealants)/patr_atual)*100
-        desvio_vale3 = ((vale3 - idealants)/patr_atual)*100
-        
-        print("Desvio percentual dos ativos:")
-        print("HGLG11: ",round(desvio_hglg11,1),"%")
-        print("HGRE11: ",round(desvio_hgre11,1),"%")
-        print("HGFF11: ",round(desvio_hgff11,1),"%")
-        print("HGPO11: ",round(desvio_hgpo11,1),"%")
-        print("WEGE3:",round(desvio_wege3,1),"%")
-        print("PETR4:",round(desvio_petr4,1),"%")
-        print("ITUB4:",round(desvio_itub4,1),"%")
-        print("VALE3:",round(desvio_vale3,1),"%")
-        
-        op = int(input("Deseja voltar ao menu inicial(1 - Sim 2 - Não): "))
-        if op == 2:
-            break
+# Calcula o patrimônio atual da carteira
+patrimonio_carteira = [q * p for q, p in zip(quantidades, current_prices)]
+
+# Calcula o valor total da carteira após o aporte
+valor_total_carteira = sum(patrimonio_carteira) + valor_aporte
+
+# Verifica se é necessário rebalancear a carteira
+if valor_total_carteira < min(current_prices):
+    print("Valor do aporte é menor que a menor cotação dos ativos.")
+else:
+    # Calcula o novo valor que cada ativo deve ter na carteira
+    novo_patrimonio = [proporcao * valor_total_carteira for proporcao in proporcoes]
+
+    # Calcula quanto falta investir em cada ativo
+    faltas = [novo_patrimonio[i] - current_prices[i] * quantidades[i] for i in range(len(ativos))]
+    print(faltas)
+    sleep(2)
+
+    # Enquanto ainda houver valor disponível para investir e a carteira estiver desbalanceada
+    while valor_aporte > min(current_prices) and any([falta > 0 for falta in faltas]):
+        # Identifica qual é o ativo com menor patrimônio atual
+        menor_patrimonio = min([current_prices[i] * quantidades[i] for i in range(len(ativos))])
+
+        # Investe o valor disponível na ação com menor patrimônio atual
+        index_menor_patrimonio = [current_prices[i] * quantidades[i] for i in range(len(ativos))].index(menor_patrimonio)
+        acao = ativos[index_menor_patrimonio]
+        quantidade_a_comprar = (faltas[index_menor_patrimonio] / current_prices[index_menor_patrimonio])
+        quantidades[index_menor_patrimonio] += quantidade_a_comprar
+        valor_aporte -= quantidade_a_comprar * current_prices[index_menor_patrimonio]
+
+        # Atualiza o patrimônio atual da carteira e as faltas de investimento
+        patrimonio_carteira = [q * p for q, p in zip(quantidades, current_prices)]
+        faltas = [novo_patrimonio[i] - current_prices[i] * quantidades[i] for i in range(len(ativos))]
+        print("Você deve comprar ",quantidade_a_comprar," cotas do ativo",ativos[index_menor_patrimonio])
+
+    # Mostra na tela as novas quantidades de ações de cada tipo
+    print("Quantidades a comprar:")
+    for i in range(len(ativos)):
+        comprar = round(quantidades[i] - quantidades1[i],2)
+        print(ativos[i],"-> ",comprar)
