b4fcda50ee030c63682e4eba4f12ec8955cd1371
Matheus Pontieri <125307539+MatheusPontieri@users.noreply.github.com>
2023-11-26 17:25:52 -0300

    Update bolsa.py

--- bolsa.py
+++ bolsa.py
@@ -1,25 +1,30 @@
 import yfinance as yf
 import heapq
-# Sempre manter yfinance atualizado
+# Sempre manter yfinance atualizado -> pip install yfinance --upgrade
 
-# Define os ativos e as proporções
-ativos = ["HGLG11.SA", "HGRE11.SA", "HGFF11.SA", "HGPO11.SA", "WEGE3.SA", "PETR4.SA", "ITUB4.SA", "VALE3.SA", "AAPL", "MSFT"]
-proporcoes = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-
-# Quantidade de ações de cada tipo
-quantidades = [2, 2, 1, 3, 10, 6, 3, 8, 0.25, 0.15]
+# Define os ativos, proporções e quantidades a partir de um arquivo txt. Não deixar linhas sobrando no arquivo
+ativos, proporcoes, quantidades, moedas, valor_moedas = [], [], [], ['USDBRL=X', 'EURBRL=X'], []
+with open('bolsa.txt', 'r') as arquivo:
+    for linha in arquivo:
+        tick, quant, prop = linha.strip().split(';')
+        ativos.append(tick), quantidades.append(float(quant)), proporcoes.append(float(prop))
 quantidades1 = quantidades.copy()
 
+print('\nCalculando cotações')
+
 # Obtém as cotações atuais dos ativos
-stock = yf.Ticker("USDBRL=X")
-dolar = stock.history(period='1d')['Close'][0]
+dados_moedas = yf.download(moedas, period = '1d', progress = False) # Posso pegar a cotação de várias moedas
+for moeda in moedas:
+    valor = dados_moedas['Close'][moeda].iloc[-1]
+    valor_moedas.append(valor)
+    
+# Posso trocar tickers = 'USDBRL=X' por uma lista e colocar [ticket] dps de Close
+info = yf.download(ativos, period = '1d', progress = False)
 
 current_prices = []
 for ticker in ativos:
-    stock = yf.Ticker(ticker)
-    current_price = stock.history(period='1d')['Close'][0]
-    if ".SA" not in ticker:
-        current_price *= dolar
+    current_price = info['Close'][ticker].iloc[-1]
+    current_price *= valor_moedas[0] if ".SA" not in ticker else 1
     current_prices.append(current_price)
 
 # Calcula o patrimônio atual da carteira e o objetivo de cada ativo
@@ -27,11 +32,13 @@ patrimonio_carteira = [q * p for q, p in zip(quantidades, current_prices)]
 prop_atual = [proporcao * sum(patrimonio_carteira) for proporcao in proporcoes]
 
 op = int(input(f"\n***Menu Interativo***\n\n[1] Mostrar o desvio percentual dos ativos\n"
-               f"[2] Rebalancear automaticamente a carteira\nSua opção: "))
+               f"[2] Rebalancear automaticamente a carteira\n"
+               f"[3] Mostrar cotação dos ativos\n"
+               f"[4] Mostrar quantidade de cada ativo\nSua opção: "))
+print()
 
 if op == 2:
     # Pede o patrimônio atual e o valor do aporte
-    print("\n")
     valor_aporte = float(input("Digite o valor do aporte: "))
     valor_aporte2 = valor_aporte
 
@@ -47,9 +54,8 @@ if op == 2:
 
         # Calcula quanto falta investir em cada ativo
         faltas = [novo_patrimonio[i] - current_prices[i] * quantidades[i] for i in range(len(ativos))]
-
-        i = 2
         # Enquanto ainda houver valor disponível para investir e a carteira estiver desbalanceada
+        i = 2
         while valor_aporte > min(current_prices) and any([falta > 0 for falta in faltas]):
             # Identifica qual é o ativo com menor patrimônio atual
             menor_patrimonio = min([current_prices[i] * quantidades[i] for i in range(len(ativos))])
@@ -69,16 +75,17 @@ if op == 2:
 
             cotacao_menor_patrimonio = current_prices[index_menor_patrimonio]
 
+            # Verificar se ativos dos Eua não entram aqui 🤔
             # Caso o programa mande comprar x cotas de um ativo para rebalancear, mas o valor restante do aporte só permite a compra de x - 1
             if valor_aporte < quantidade_a_comprar * cotacao_menor_patrimonio:
                 print(f"O aporte restante não permite comprar {quantidade_a_comprar} cotas de {ativos[index_menor_patrimonio]}, haverá uma diminuição de cotas até poder comprar!")
                 while valor_aporte < quantidade_a_comprar * cotacao_menor_patrimonio or quantidade_a_comprar == 0:
                     # Se mandar comprar 1 cota e depois verificar que não tenho dinheiro nem para uma, vai diminuir para 0
                     quantidade_a_comprar -= 1
-                    if quantidade_a_comprar <= 0: # Só de entrar nesse if, já não tenho mais dinheiro para comprar
+                    if quantidade_a_comprar <= 0: #Só de entrar nesse if, já não tenho mais dinheiro para comprar
                         proximo_menor_patrimonio = heapq.nsmallest(i, menor_patrimonio1)[-1]
                         index_menor_patrimonio = menor_patrimonio1.index(proximo_menor_patrimonio)
-                        quantidade_a_comprar = round(faltas[index_menor_patrimonio] / current_prices[index_menor_patrimonio])
+                        quantidade_a_comprar = round(faltas[index_menor_patrimonio] / current_prices[index_menor_patrimonio]) # Visc vai dar 0 aqui
                         cotacao_menor_patrimonio = current_prices[index_menor_patrimonio]
                         if cotacao_menor_patrimonio < valor_aporte and quantidade_a_comprar <= 0:
                             quantidade_a_comprar = 1
@@ -98,10 +105,11 @@ if op == 2:
         # Mostra na tela as novas quantidades de ações de cada tipo
         print("\nQuantidades a comprar:\n")
         for i in range(len(ativos)):
-            print(r"Comprar",quantidades[i] - quantidades1[i],"",ativos[i])
+            comprar = quantidades[i] - quantidades1[i]
+            if comprar != 0:
+                print(f"Comprar {comprar} {ativos[i]}")
 
 elif op == 1:
-    print("")
     lista_desvio = []
     for i in range(len(ativos)):
         desvio = (patrimonio_carteira[i] - prop_atual[i])/sum(patrimonio_carteira)*100
@@ -113,5 +121,13 @@ elif op == 1:
         index_prox_menor_pctg = lista_desvio.index(proximo_menor_pctg)
         print(f"{ativos[index_prox_menor_pctg]}: {round(proximo_menor_pctg,1)}%")
         i += 1
+
+elif op == 3:
+    print(*(f'{ativos[i]}: R$ {current_prices[i]:.2f}' for i in range(len(current_prices))), sep='\n')
+
+elif op == 4:
+    for i in range(len(ativos)):
+        print(f"{ativos[i][:6]}: {quantidades1[i]}")
+
 else:
     print("Opção Inválida!")
